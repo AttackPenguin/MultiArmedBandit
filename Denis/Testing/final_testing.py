e5607a5296dfd9d0c108db24eb76a99820f52d4d
AttackPenguin <denis.e.hudon@gmail.com>
2022-05-12 17:01:31 -0400

    Working on data analysis

--- Denis/Testing/final_testing.py
+++ Denis/Testing/final_testing.py
@@ -19,7 +19,15 @@ sns.set()
 
 
 def main():
-    model = torch.load('model_weights_training_window_1285_loss_0.25232.pth')
+    model = MABInceptionModel2(
+        use_dropout=True,
+        dropout_ratio=0.05,
+        use_batch_norm=True
+    )
+    model.load_state_dict(
+        torch.load('model_weights_training_window_1285_loss_0.25232.pth')
+    )
+    model.eval()
     get_model_rewards(model, get_squished_test_gens())
 
 
@@ -31,7 +39,6 @@ def get_model_rewards(
     for i in range(0, len(reward_generators), 1000):
         _, _, local_rewards = model(reward_generators[i:i+1000])
         rewards += local_rewards
-
 
 
 if __name__ == '__main__':
