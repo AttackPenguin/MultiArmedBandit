17394a1c176306394744cf3bbfacfa609269ddec
AttackPenguin <denis.e.hudon@gmail.com>
2022-05-12 16:28:47 -0400

    Working on data analysis

--- Denis/Testing/final_testing.py
+++ Denis/Testing/final_testing.py
@@ -9,6 +9,7 @@ import numpy as np
 import seaborn as sns
 import torch
 
+from Denis.Testing.testing_generators import get_baseline_generators
 from Denis.nn_models import MABInceptionModel2
 from Denis.reward_generators import RewardGeneratorChallenging
 
@@ -18,17 +19,17 @@ sns.set()
 
 
 def main():
-    g_training_loss(
-        "/home/denis/PycharmProjects/MultiArmedBandit/Denis/Experiment 03/2022-03-26 10:09:18",
-        True, 14900
-    )
+    pass
 
 
 def get_model_rewards(
         model: MABInceptionModel2,
         reward_generators: list[RewardGeneratorChallenging]
 ):
-
+    rewards = list()
+    for i in range(0, len(reward_generators), 1000):
+        _, _, local_rewards = model(reward_generators[i:i+1000])
+        rewards += local_rewards
 
 
 
